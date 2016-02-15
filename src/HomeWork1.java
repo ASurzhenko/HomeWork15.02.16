24c9fd0a82eac5f175270c4edbfeff42501b4dda
Александр <britishop@gmail.com>
2016-02-15 21:22:58 +0200

    HomeWork1

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,17 +1,20 @@
 /**
  * Created by Александр on 15.02.2016.
  */
+
+// Найти минимальное из 3х чисел, причем, если минимальных несколько вывести все такие числа
+
 public class HomeWork1 {
     public static void main(String[] args) {
         int v1 = 100;
         int v2 = 10;
         int v3 = 10;
 
-        if(v1 <= v2 && v1 <= v3)
+        if (v1 <= v2 && v1 <= v3)
             System.out.println("Min is " + v1);
-        if(v2 <= v1 && v2 <= v3)
+        if (v2 <= v1 && v2 <= v3)
             System.out.println("Min is " + v2);
-        if(v3 <= v1 && v3 <= v2)
+        if (v3 <= v1 && v3 <= v2)
             System.out.println("Min is " + v3);
     }
 }
---
+++ src/HomeWork2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Александр on 15.02.2016.
+ */
+
+// Написать программу, которая проверит, является ли число четным или нечетным
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        int v1 = 3;
+
+        if (v1 % 2 == 0) {
+            System.out.println("The number " + v1 + " is even");
+        } else {
+            System.out.println("The number " + v1 + " is not even");
+        }
+    }
+}
