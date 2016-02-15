0720d445184fe98a5364d5d0056f65c91b55b63f
Александр <britishop@gmail.com>
2016-02-15 21:13:19 +0200

    HomeWork1

---
+++ src/HomeWork1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Александр on 15.02.2016.
+ */
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int v1 = 100;
+        int v2 = 10;
+        int v3 = 10;
+
+        if(v1 <= v1 && v1 <= v3)
+            System.out.println("Min is " + v1);
+    }
+}
