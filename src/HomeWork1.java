25912dcd56f11481bb3ae593f188a133b82c4f76
Александр <britishop@gmail.com>
2016-02-15 21:14:30 +0200

    HomeWork1

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -7,7 +7,11 @@ public class HomeWork1 {
         int v2 = 10;
         int v3 = 10;
 
-        if(v1 <= v1 && v1 <= v3)
+        if(v1 <= v2 && v1 <= v3)
             System.out.println("Min is " + v1);
+        if(v2 <= v1 && v2 <= v3)
+            System.out.println("Min is " + v2);
+        if(v3 <= v1 && v3 <= v2)
+            System.out.println("Min is " + v3);
     }
 }
